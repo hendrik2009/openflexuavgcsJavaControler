ef4087ade0f20dcfeae4c0cfaa6033e64932277f
hendrik2009 <kontakt@hendrikkroeger.de>
2012-02-21 19:32:11 +0100

    Main - Ardrone connection established and send first controlles

--- src/OpenFlexJavaContoller.java
+++ src/OpenFlexJavaContoller.java
@@ -36,29 +36,85 @@ public class OpenFlexJavaContoller {
 		ControllerEventManager.addControllerEventListener( _jsDataProvider );
 		
 		//connecting Drone
-		//_drone 						= new ARDrone();
+		_drone 						= new ARDrone("192.168.1.1");
+		System.out.println("connect drone controller");
+		_drone.connect();
+		System.out.println("connect drone navdata");
+		_drone.connectNav();
+		System.out.println("connect drone video");
+		_drone.connectVideo();
+		System.out.println("start drone");
+		_drone.start();
 		
 		// Controlldata Loop pulling Data every 1000 ms to set NavigationCommand 
 		do {
 			
 			try {
-				Thread.sleep(1000); // check once every second -> wait 1000ms
+				Thread.sleep(100); // check once every second -> wait 1000ms
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 				}
 			System.out.println("tick");
-			for ( float item :_jsDataProvider.getJsData()){
-				System.out.println(item);
-			}
+			navigateByDirectControl( _jsDataProvider.getJsData() );
+				
 			System.out.println("tickEND");
 			System.out.println("");
 					//_jsDataProvider.getJsData()[0]);
 		} while (true );
-		
 	}
 	
 	// Constructor
 	public void OpenFlexJavaControler(String[] arg){
+		
+	}
+	
+	static void navigateByDirectControl(float[] jsData){
+		
+		/*
+		// Left right on x-axis
+		if( jsData[0] > 0){
+			System.out.println("rightStrave");
+			_drone.goRight(1);
+		}
+		else if( jsData[0] < 0){
+			System.out.println("leftStrave");
+			_drone.goLeft(1);
+		}
+		else{
+			_drone.stop();
+		}
+		
+		// For- Back -wards on y-axis
+		if( jsData[1] > 0){
+			System.out.println("BACK");
+		}
+		else if( jsData[1] < 0){
+			System.out.println("FORWARD");
+		}
+		*/
+		
+		
+		// start on btn 0
+		if( jsData[4] > 0 ){
+			System.out.println("start");
+			_drone.takeOff();
+		}
+		// land on btn 1
+		if( jsData[5] > 0 ){
+			System.out.println("land");
+			_drone.landing();
+		}
+		
+		// start on btn 0
+		if( jsData[6] > 0 ){
+			System.out.println("l");
+			_drone.spinLeft(100);
+		}
+		// land on btn 1
+		if( jsData[7] > 0 ){
+			System.out.println("r");
+			_drone.spinRight(100);
+		}
 		
 	}
 }
